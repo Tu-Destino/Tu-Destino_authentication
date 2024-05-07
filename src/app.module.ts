fe92052db4e1ee5b40c3fa9e7ed88fcc90dc0d51
holas1356 <ruthbustamante075@gmail.com>
2024-05-07 08:53:16 -0500

    docs: changes at users.module, entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './modules/users/users.module';
+import dbConfig from './libs/persistance/db-config';
+import { ConfigModule } from '@nestjs/config';
+import { PersistenceModule } from './libs/persistance/persistance.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    PersistenceModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+/* import { UpdateUserDto } from './dto/update-user.dto'; */
 
 @Injectable()
 export class UsersService {
@@ -16,7 +16,7 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: number, updateUserDto: any) {
     return `This action updates a #${id} user`;
   }
 
