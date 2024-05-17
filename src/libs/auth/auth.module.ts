f6590553cee7b507e494c138801b2507ca4266d7
holas1356 <ruthbustamante075@gmail.com>
2024-05-17 12:41:37 -0500

    Feat: Changes with errors managments

--- src/libs/auth/auth.module.ts
+++ src/libs/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthService } from './services/auth.service';
 import { UsersModule } from 'src/modules/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { UtilsModule } from '../utils/utils.module';
+import { ErrorModule } from '../errors/error.module';
 
 
 @Module({
   imports:[
+    ErrorModule,
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
--- src/modules/users/services/users.service.ts
+++ src/modules/users/services/users.service.ts
@@ -3,24 +3,21 @@ import { CreateUserDto } from '../dto/create-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../entities/user.entity';
 import { Model } from 'mongoose';
+import { ErrorService } from 'src/libs/errors/error.service';
 /* import { UpdateUserDto } from './dto/update-user.dto'; */
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
+  constructor(@InjectModel(User.name) 
+  private readonly userModel: Model<User>
+
+) {}
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userModel.create(createUserDto);
   }
 
 
-  async findOneByEmailRegister(email: string): Promise<User> {
-    const user = await this.userModel.findOne({ email }).exec();
-    if (user) {
-      throw new NotFoundException(`User with email ${email} already exists`);
-    }
-    return user;
-  }
 
   async findOneByEmail(email: string): Promise<User> {
     const user = await this.userModel.findOne({ email }).exec();
