2eb57f84c512152e4f9b623ccce194ba605a6d90
holas1356 <ruthbustamante075@gmail.com>
2024-05-19 10:56:11 -0500

    anable cors

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1/api', { exclude: ['/'] });
   app.enableCors({
-    origin: '*', 
+    origin: 'https://tu-destino-authentication.onrender.com', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
   const port = process.env.PORT || 4000;
 
   const config = new DocumentBuilder()
