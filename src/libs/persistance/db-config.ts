6ebdde0006769a4432ab82ae77d6a3467f71ddc7
Kristianmx <155922266+Kristianmx@users.noreply.github.com>
2024-05-21 13:52:54 -0500

    Update db-config.ts Mongo

--- src/libs/persistance/db-config.ts
+++ src/libs/persistance/db-config.ts
@@ -3,14 +3,14 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('dbConfig', () => {
   const dbConfig = {
     db: {
-      connection: process.env.DB_CONNECTION,
-      host: process.env.DB_HOST,
-      name: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      
+      connection: "mongodb+srv://",
+      host: "localhost:27017",
+      name: "tuDestino",
+      user: "tuDestino",
+      password: "Tu.Destino.24",
+        
     },
-    env: process.env.NODE_ENV || 'local',
+    env:'production',
   };
   return dbConfig;
 });
